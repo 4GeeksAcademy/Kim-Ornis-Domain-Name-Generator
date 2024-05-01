29a8a2cd325ee4878baa81a495ee98d0714cc867
KOTHECODE <160642762+KOTHECODE@users.noreply.github.com>
2024-05-01 01:46:36 +0000

    submit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["KO", "Kimbo"];
+  let adj = ["Amazing", "Effecient"];
+  let noun = ["Coder", "Accountant"];
+
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        let ranDo = document.createElement("ranDo");
+        ranDo.innerText = `${pronoun[a]}${adj[b]}${noun[c]}.com`;
+
+        console.log(ranDo);
+        document.body.appendChild(ranDo);
+      }
+    }
+  }
 };
